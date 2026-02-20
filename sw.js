570b6298bef3f6d12c35e487db483a234e552eb5
Incipient <incipient@Incipients-MacBook-Air.local>
2026-02-19 19:19:43 -0500

    NREMT Advanced Exam Prep - 6 question types, progressive scenarios, glass design

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'nremt-prep-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
